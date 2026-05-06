8a8f2c94d3e196e066c1d2c59eb09d149b619c8d
Karthikeyan Marimuthu <karthikm2010@gmail.com>
2026-05-06 16:50:47 +0200

    4 script added  .

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square class with size validation and area computation."""
+
+
+class Square:
+    """Represent a square with a validated private size attribute."""
+
+    def __init__(self, size=0):
+        """Initialize the square with an optional size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square after validation."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
