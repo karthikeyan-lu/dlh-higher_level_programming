8aefc70c46c967ea81ada9a76a0ade0b390b55c1
Karthikeyan Marimuthu <karthikmarimuthu.dev@gmail.com>
2026-05-07 16:53:03 +0200

    0 script updated

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module for reading a text file."""
+
+
 def read_file(filename=""):
-    """Reads a UTF8 text file and prints it to stdout."""
+    """Reads a UTF-8 text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
